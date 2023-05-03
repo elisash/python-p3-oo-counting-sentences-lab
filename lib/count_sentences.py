5b81b2280d19efac84f12ea261bd37327af39e63
elisa-sh <126708543+elisa-sh@users.noreply.github.com>
2023-05-03 14:53:57 -0400

    ended with count_sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+  
+  def get_value(self):
+    return self.value
+  
+  def set_value(self, string):
+    if(type(string)== str):
+      self.value = string
+    else:
+      print("The value must be a string.")
+  
+  value = property(get_value, set_value)
+
+  def is_sentence(self, value):
+    if value[-1] == ".":
+       return True
+    else: 
+       return False
+  
+  def is_question(self, value):
+    if value[-1] == "?":
+      return True
+    else: 
+      return False
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+
+  
